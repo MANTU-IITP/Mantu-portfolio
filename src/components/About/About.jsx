92ec060e36b4518036dc51ba54f69d0f7b2195b1
manturoy440 <mantukumar823512@gmail.com>
2025-05-16 13:54:40 +0530

    resume link chnged

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1ico1WfVcPbxbpaZ_yjfsxn3IUo6GYCh1/view?usp=sharing"
+            href="https://drive.google.com/file/d/1MCalIz3ZO3gpciFh4xdJ9tXU2U3vipj9/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
